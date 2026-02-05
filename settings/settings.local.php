9f6bb5053a3e2147b4bb0bf319fbe6d5adf34b87
Renier John Sediego <renierjohnsediego@gmail.com>
2026-02-05 14:38:09 +0800

    Install metatag & sitemap

--- settings/settings.local.php
+++ settings/settings.local.php
@@ -1,13 +1,13 @@
 <?php
 
-// phpcs:ignoreFile
-$settings['hash_salt'] = 'SkTL5ABUi5IqR5tTvFho5gc6SLYI93QzlJVOgx-zZhUvewfRIJCQx0A5MoEKb_Xi_yzjTxRwrA';
-$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
- $settings['trusted_host_patterns'] = [
-    '^events-ph\.com$',
-    '^.+\.events-ph\.com$',
-    '^192.168.1.3$',
- ];
+$settings['trusted_host_patterns'] = [
+  '^events-ph\.com$',
+  '^.+\.events-ph\.com$',
+  '^.+\.dupr\ddev\.site$',
+  '^192.168.1.3$',
+];
 
 $settings['config_sync_directory'] = '../config/default';
-$config["config_split.config_split.dev"]["status"] = TRUE;
+// $config["config_split.config_split.dev"]["status"] = TRUE;
+// $config['system.performance']['css']['preprocess'] = FALSE;
+// $config['system.performance']['js']['preprocess'] = FALSE;
